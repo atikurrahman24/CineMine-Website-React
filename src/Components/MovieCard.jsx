d001080686eb26a75ab0dece4db554746cbd2310
atik <thtatikurrahman24@gmail.com>
2025-11-06 03:53:28 +0600

    Updated movie card design

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -8,11 +8,11 @@ const MovieCard = ({ movie }) => {
 
 
     return (
-        <div className='font-mon transition-transform duration-300 hover:scale-105 flex flex-col border border-yellow-500 rounded-2xl gap-3 '>
+        <div className='font-mon transition-transform duration-300 hover:scale-105 flex flex-col border border-gray-700 rounded-2xl gap-3 py-3'>
             <img src={card_picture} alt={title} className=' h-70 mx-auto object-contain rounded-lg' />
-            <div className='py-1 flex flex-col gap-1'>
+            <div className='py-1 flex flex-col gap-1 ho'>
                 <div>
-                    <div className='details flex justify-between px-16 sm:px-8 md:px-6 lg:px-10 xl:px-12'>
+                    <div className='details flex justify-between px-10 sm:px-6 md:px-6 lg:px-10 xl:px-12'>
                         <h3>{title}</h3>
                         <div className='flex gap-2 py-1'>
                             <FaEye className='text-gray-300' />
@@ -20,8 +20,8 @@ const MovieCard = ({ movie }) => {
                         </div>
                     </div>
                 </div>
-                <p className='text-sm text-gray-400 px-16 sm:px-8 md:px-6 lg:px-10 xl:px-12'>{released_date}</p>
-                <div className='flex justify-between px-16 sm:px-8 md:px-6 lg:px-10 xl:px-12'>
+                <p className='text-sm text-gray-400 px-10 sm:px-6 md:px-6 lg:px-10 xl:px-12'>{released_date}</p>
+                <div className='flex justify-between px-10 sm:px-6 md:px-6 lg:px-10 xl:px-12'>
                     <p className='text-sm text-black font-extrabold bg-yellow-400 rounded px-2'>{platform}</p>
                     <p className='flex justify-center items-center gap-2 text-sm text-gray-400'>
                         <FaStarHalfAlt className='text-yellow-400' />
--- src/Components/MovieGalary.jsx
+++ src/Components/MovieGalary.jsx
@@ -4,64 +4,74 @@ import toast from 'react-hot-toast';
 import MovieCard from './MovieCard';
 
 const MovieGalary = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    // A loading theme when fetching movies data
-    const [loading, setLoading] = useState(true);
+  // A loading theme when fetching movies data
+  const [loading, setLoading] = useState(true);
 
-    // Keep the value of selected Category
-    const [selectCategory, setSelectCategory] = useState("All");
+  // Keep the value of selected Category
+  const [selectCategory, setSelectCategory] = useState("All");
 
-    // Movie data card showing limitation 8/all
-    const [showAll, setShowAll] = useState(false)
+  // Movie data card showing limitation 8/all
+  const [showAll, setShowAll] = useState(false)
 
-    useEffect(()=>{
-        fetch('/Movies.json')
-        .then(res => res.json())
-        .then(data => setMovies(data))
+  useEffect(() => {
+    fetch('/Movies.json')
+      .then(res => res.json())
+      .then(data => setMovies(data))
 
-        // If failed to fetch data then a toast message
-        .catch((error)=>{
-            toast.error("Failed to load Movie Data!!!")
-        })
+      // If failed to fetch data then a toast message
+      .catch((error) => {
+        toast.error("Failed to load Movie Data!!!")
+      })
 
-        // After fetching data successfully no needs to Loading
-        .finally(() => setLoading(false))
+      // After fetching data successfully no needs to Loading
+      .finally(() => setLoading(false))
 
-    },[])
+  }, [])
 
-    const categories =["All", ...new Set(movies.map(m => m.category))]
-    const filterMovies = selectCategory === "All" ? movies : movies.filter(m => m.category === selectCategory)
-    const visibleMovies = showAll ? filterMovies : filterMovies.slice(0,10)
+  const categories = ["All", ...new Set(movies.map(m => m.category))]
+  const filterMovies = selectCategory === "All" ? movies : movies.filter(m => m.category === selectCategory)
+  const visibleMovies = showAll ? filterMovies : filterMovies.slice(0, 10)
 
   return (
     <div className='w-11/12 mx-auto py-10'>
-        <p className='text-lg lg:text-xl'>RECOMANDED FOR YOU</p>
-        <CategoriesNav 
+      <p className='text-lg lg:text-2xl'>RECOMANDED FOR YOU</p>
+      <CategoriesNav
         categories={categories}
         selectCategory={selectCategory}
         setSelectCategory={setSelectCategory}
-        ></CategoriesNav>
-        {
-          loading ? 
+      ></CategoriesNav>
+      {
+        loading ?
           (<div className='flex justify-center items-center h-64'>
             <span className="loading loading-dots loading-xl text-yellow-500"></span>
           </div>)
           : filterMovies.length > 0 ?
-          (
-            <>
-              <div className='grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6'>
+            (
+              <>
+                <div className='grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10 mx-8'>
+                  {
+                    visibleMovies.map(movie => (
+                      <MovieCard key={movie.id} movie={movie}></MovieCard>
+                    ))
+                  }
+                </div>
                 {
-                  visibleMovies.map(movie => (
-                    <MovieCard key={movie.id} movie={movie}></MovieCard>
-                  ))
+                  filterMovies.length > 10 && (
+                    <div className='flex justify-center mt-8'>
+                      <button
+                      className='px-6 py-2 bg-yellow-500 text-black font-bold rounded-full hover:bg-yellow-400 transition-all duration-300'
+                      onClick={()=>setShowAll(!showAll)}
+                      >{showAll?"Show Less":"Show More"}</button>
+                    </div>
+                  )
                 }
-              </div>
-            </>
-          ) : (
-            <p className='text-gray-400 text-center'>No Movies Found</p>
-          )
-        }
+              </>
+            ) : (
+              <p className='text-gray-400 text-center'>No Movies Found</p>
+            )
+      }
     </div>
   )
 }
